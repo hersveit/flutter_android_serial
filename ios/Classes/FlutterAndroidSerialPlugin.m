f4c39877ca7075f0d31969efc4d05fdbe852e03b
Ivan Cherkasov <cherkasov.ivan@yahoo.com>
2019-02-28 18:30:28 +0700

    remove swift;

--- ios/Classes/FlutterAndroidSerialPlugin.m
+++ ios/Classes/FlutterAndroidSerialPlugin.m
@@ -1,8 +1,20 @@
 #import "FlutterAndroidSerialPlugin.h"
-#import "SwiftFlutterAndroidSerialPlugin-Swift.h"
 
 @implementation FlutterAndroidSerialPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterAndroidSerialPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_android_serial"
+            binaryMessenger:[registrar messenger]];
+  FlutterAndroidSerialPlugin* instance = [[FlutterAndroidSerialPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getAndroidSerial" isEqualToString:call.method]) {
+    result("");
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
