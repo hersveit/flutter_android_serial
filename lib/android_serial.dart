2a330f1535a1ae0c1f5d8eb97cadbf7be71d98d7
Ivan Cherkasov <cherkasov.ivan@yahoo.com>
2019-02-28 13:02:10 +0700

    add description to getter

--- lib/android_serial.dart
+++ lib/android_serial.dart
@@ -6,6 +6,9 @@ class AndroidSerial {
   static const MethodChannel _channel =
       const MethodChannel('android_serial');
 
+  /// Returns android.os.Build.SERIAL
+  /// 
+  /// Returns empty string on iOS
   static Future<String> get androidSerialNumber async {
     final String version = await _channel.invokeMethod('getSerialNumber');
     return version;
