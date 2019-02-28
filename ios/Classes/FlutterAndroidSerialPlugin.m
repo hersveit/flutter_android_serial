aba915eda4ff0a6407483024c02b662477e3ed18
Ivan Cherkasov <cherkasov.ivan@yahoo.com>
2019-02-28 18:32:13 +0700

    fix result;

--- ios/Classes/FlutterAndroidSerialPlugin.m
+++ ios/Classes/FlutterAndroidSerialPlugin.m
@@ -11,7 +11,7 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getAndroidSerial" isEqualToString:call.method]) {
-    result("");
+    result(@"");
   } else {
     result(FlutterMethodNotImplemented);
   }
