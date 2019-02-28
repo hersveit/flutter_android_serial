9253b318905f03764fb0b482fd6eaa193a151031
Ivan Cherkasov <cherkasov.ivan@yahoo.com>
2019-02-28 12:59:38 +0700

    return empty string on iOS

--- ios/Classes/SwiftAndroidSerialPlugin.swift
+++ ios/Classes/SwiftAndroidSerialPlugin.swift
@@ -9,6 +9,6 @@ public class SwiftAndroidSerialPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    result("")
   }
 }
